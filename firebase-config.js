da7885eec2fe291cf7a9b93eddc449580d835f1e
Scott Martin <scott.martin88@live.com>
2023-12-12 13:11:22 +0100

    Refactor firebase-config file

---
+++ firebase-config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCf_ixKclZTGemkFJGZTzT1f8VodJI-EcY",
+  authDomain: "flashcard-app-ef223.firebaseapp.com",
+  projectId: "flashcard-app-ef223",
+  storageBucket: "flashcard-app-ef223.appspot.com",
+  messagingSenderId: "189058797870",
+  appId: "1:189058797870:web:bcc3be1919391581c0eabf",
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore();
+
+export { app, db };
