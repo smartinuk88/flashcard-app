ad0fc403c80724dbb7d259579cef7be05e9ec917
Scott Martin <scott.martin88@live.com>
2024-01-09 10:53:08 +0100

    Structure basic sign in component

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,18 +1,19 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { handleSignInWithGoogle } from "../firebase-config";
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
 
 function SignIn() {
   return (
-    <div className="flex h-screen w-full max-w-6xl m-auto justify-center md:p-20 ">
-      <div className="flex flex-col space-y-5 justify-center spay items-center w-full lg:w-72 bg-gradient-to-tl from-primary-blue to-secondary-blue rounded-3xl">
-        <img src="" alt="" />
-        <h1 className="font-bold text-3xl lg:text-5xl text-white">Flip</h1>
-        <button
-          className="py-4 rounded-full w-full"
-          onClick={handleSignInWithGoogle}
-        >
-          Sign in with Google
-        </button>
-      </div>
+    <div className="flex flex-col space-y-8 h-screen w-screen pt-28 items-center bg-gradient-to-tl from-primary-blue to-secondary-blue">
+      <img src="" alt="" />
+      <h1 className="font-bold text-5xl md:text-7xl text-white">FlipFluent</h1>
+      <button
+        className="p-4 flex justify-center font-semibold items-center rounded-full bg-white w-3/5 lg:w-2/5 shadow-sm hover:shadow-md transition duration-75"
+        onClick={handleSignInWithGoogle}
+      >
+        <FontAwesomeIcon icon={faGoogle} className="text-3xl mr-5" /> Sign in
+        with Google
+      </button>
     </div>
   );
 }
