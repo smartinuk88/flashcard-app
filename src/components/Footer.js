9a89889dc01b8c655c5fca89aea6628cab27bcfb
Scott Martin <scott.martin88@live.com>
2024-06-14 23:06:28 +0200

    Fix small issue with height of DeckDash, ensuring it is the height of the screen

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,14 @@
+import { Link } from "react-router-dom";
+
 function Footer() {
   return (
     <footer className="flex justify-between items-center w-full h-12 px-6 py-3 bg-white">
-      <p className="text-lg">
-        Flash<span className="text-one">Fluent</span>
-      </p>
-      <p className="text-sm">Copyright Scott Martin 2024</p>
+      <Link to={"/"}>
+        <p className="md:text-lg">
+          Flash<span className="text-one">Fluent</span>
+        </p>
+      </Link>
+      <p className="text-xs md:text-sm">Copyright Scott Martin 2024</p>
     </footer>
   );
 }
